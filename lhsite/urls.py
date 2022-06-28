44cb41b504b0b8dde2eb5930203157f0c92f4935
Matthew-Rockhill <admin@rocksolid.agency>
2022-06-28 22:48:04 +0200

    static added

--- lhsite/urls.py
+++ lhsite/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import views
@@ -10,3 +12,6 @@ urlpatterns = [
     path('sermons/', views.sermons, name='sermons'),
     path('contact-us/', views.contact_us, name='contact-us'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=setting.STATIC_ROOT)
